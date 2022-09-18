a889d0c6b0cef4de89b163daecdc94005ee762d3
Vimal Kumar <77979365+vimalkumar-2124@users.noreply.github.com>
2022-09-18 15:56:26 +0530

    Delete Express-gen directory

--- Express-gen/dbconfig.js
+++
@@ -1,7 +0,0 @@
-const mongodb = require('mongodb')
-const dbName = 'users'
-const url = `mongodb://localhost:27017/${dbName}`
-const client = mongodb.MongoClient
-
-module.exports = {dbName, url, client, mongodb}
-
